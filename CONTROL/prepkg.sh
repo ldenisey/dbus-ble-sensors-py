478071de90c41abb4913e2cffe4f01efce281b34
ldenisey <ldenisey@gmail.com>
2025-11-19 14:30:34 +0000

    Fix prepkg

--- CONTROL/prepkg.sh
+++ CONTROL/prepkg.sh
@@ -2,4 +2,7 @@
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Setting shell rights
-chmod +x "$SCRIPT_DIR/postinst" "$SCRIPT_DIR/postrm" "$SCRIPT_DIR/../opt/victronenergy/service/dbus-ble-sensors-py/*.sh"
+chmod +x "$SCRIPT_DIR/postinst" "$SCRIPT_DIR/postrm"
+chmod +x "$SCRIPT_DIR/../opt/victronenergy/dbus-ble-sensors-py/start-dbus-ble-sensors-py.sh"
+chmod +x "$SCRIPT_DIR/../opt/victronenergy/service/dbus-ble-sensors-py/run"
+chmod +x "$SCRIPT_DIR/../opt/victronenergy/service/dbus-ble-sensors-py/log/run"
